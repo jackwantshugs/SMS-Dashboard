a5ea1b68350f36d38663e5250f4b9619c86bce84
Jack Spence <jack.xenophelius.spence@gmail.com>
2014-10-19 13:15:18 +0200

    Posting to twilio

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ $(document).ready(function(){
     Mousetrap.bind('ctrl+enter', function(e) {    
         if(e.target === $inputBox[0]){
             var sentMessage = $inputBox.text();
-            console.log(sentMessage); // Where we can plug in the code to send message to be sent
+            //console.log(sentMessage); // Where we can plug in the code to send message to be sent
+            $.post( "http://jakemulley.webfactional.com/api/1.0/send", { number: "+447591658146", message: sentMessage } );
             $inputBox.html('');
             var dt = new Date();
             var time = dt.getHours() + ":" + dt.getMinutes();
